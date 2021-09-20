b3f14cb5ed4b165b27b2f35a3d21b32281b4743c
whitewolf8383 <kandt.vandf@gmail.com>
2021-09-20 14:06:02 -0600

    Learning React Native - #6

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Keith');
+  const [age, setAge] = useState('38');
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>name: {name} age: {age}</Text>
+      <Text>Enter Name:</Text>
+      <TextInput 
+      style={styles.input} 
+      placeholder='Enter your name'
+      onChangeText={(val) => {setName(val)}} />
+
+      <Text>Enter Age:</Text>
+      <TextInput 
+      keyboardType='numeric'
+      style={styles.input} 
+      placeholder='Enter your age'
+      onChangeText={(val) => {setAge(val)}} />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +35,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
   },
 });
